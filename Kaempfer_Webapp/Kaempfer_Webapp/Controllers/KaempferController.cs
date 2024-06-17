b249741557c534f866b1a1cfabbc3bcf3675f974
hotdogdealer <samuil.georgiev@bulme.at>
2024-06-17 23:22:47 +0200

    asd

--- Kaempfer_Webapp/Kaempfer_Webapp/Controllers/KaempferController.cs
+++ Kaempfer_Webapp/Kaempfer_Webapp/Controllers/KaempferController.cs
@@ -1,6 +1,7 @@
 using Kaempfer_Webapp.Context;
 using Kaempfer_Webapp.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Kaempfer_Webapp.Controllers;
 
@@ -13,7 +14,7 @@ public class KaempferController : Controller
          MyDbContext dbctx = new MyDbContext();
          
          //Alle Kaempfer Items auslesen
-         List<Kaempfer?> kaempfer = dbctx.Kaempfer.ToList();
+         List<Kaempfer?> kaempfer = dbctx.Kaempfer.ToList();    
          //Der View übergeben
         return View(kaempfer);
     }
@@ -26,11 +27,25 @@ public class KaempferController : Controller
         dbctx.SaveChanges();
         return RedirectToAction("Index");
     }
+    [HttpPost]
     public IActionResult UpdateKaempfer(Kaempfer? kaempfer)
     {
+        if (kaempfer == null)
+        {
+            return BadRequest();
+        }
+
         MyDbContext dbctx = new MyDbContext();
-        dbctx.Kaempfer.Update(kaempfer);
-        dbctx.SaveChanges();
+        var existingKaempfer = dbctx.Kaempfer.FirstOrDefault(k => k.Kaempferid == kaempfer.Kaempferid);
+
+        if (existingKaempfer != null)
+        {
+            existingKaempfer.Vorname = kaempfer.Vorname;
+            existingKaempfer.Nachname = kaempfer.Nachname;
+            existingKaempfer.Geburtsdatum = kaempfer.Geburtsdatum;
+            dbctx.SaveChanges();
+        }
+
         return RedirectToAction("Index");
     }
     public IActionResult New()
@@ -41,11 +56,15 @@ public class KaempferController : Controller
     public IActionResult Edit(Guid id)
     {
         MyDbContext dbctx = new MyDbContext();
-        Kaempfer? kaempfer = dbctx.Kaempfer.FirstOrDefault(b => b != null && b.Kaempferid == id);
+        Kaempfer kaempfer = dbctx.Kaempfer.FirstOrDefault(b => b.Kaempferid == id);
         return View(kaempfer);
     }
     public IActionResult Delete(Guid id)
     {
+        MyDbContext dbctx = new MyDbContext();
+        Kaempfer kaempferToDelete = dbctx.Kaempfer.FirstOrDefault(b => b.Kaempferid == id);
+        dbctx.Kaempfer.Remove(kaempferToDelete);
+        dbctx.SaveChanges();
         return RedirectToAction("Index");
     }
 }
